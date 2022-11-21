421b804ae7cdb8b0e957bec5f0f19b062fbf2d8b
Danpa_cho <danpa725@cau.ac.kr>
2022-11-21 22:37:25 +0900

    :recycle: refactor: extract utility functions

--- src/engine/util.js
+++ src/engine/util.js
@@ -1,9 +1,29 @@
 /**
+ * Random number(`int`) between min max
  * @param {number} min
  * @param {number} max
  * @returns {number}
  */
-const numBetween = (min, max) =>
+const randBetweenMinMax = (min, max) =>
     Math.floor(Math.random() * (max - min + 1) + min)
 
-export { numBetween }
+/**
+ * Render square with specific 2D ctx
+ * @param {{coord: [number, number], fill: string, ctx: CanvasRenderingContext2D, size: number}} param0
+ */
+const renderSquare = ({ coord, fill, ctx, size }) => {
+    ctx.fillStyle = fill
+    ctx.fillRect(coord[0], coord[1], size, size)
+}
+
+/**
+ * Get random grid coordinate
+ * @param {{gridSize: number, gridLimit: number}} arg
+ * @returns {[number, number]}
+ */
+const getRandomGridCoord = ({ gridSize, gridLimit }) => [
+    gridSize * randBetweenMinMax(0, gridLimit),
+    gridSize * randBetweenMinMax(0, gridLimit),
+]
+
+export { randBetweenMinMax, renderSquare, getRandomGridCoord }
